b2f6588a990907861f7bcd240264ec324b8610f7
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-12 18:25:35 +0530

    routes modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Login } from './pages/Login.jsx'
 import { Dashboard } from './pages/Dashboard.jsx'
 import { AddUser } from './pages/AddUser.jsx'
 import { FinanceDashboard } from './pages/FinanceDashboard.jsx'
+import { GeneralDashboard } from './pages/GeneralDashboard.jsx'
 function App() {
 
   return (
@@ -11,10 +12,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} >
+            <Route path='finance' element={<FinanceDashboard />} />
+            <Route path='general' element={<GeneralDashboard />} />
+            <Route path='user' element={<AddUser />} />
+          </Route>
           <Route path='/login' element={<Login />} />
-          <Route path='/add-user' element={<AddUser />} />
-          <Route path='/finance-dashboard' element={<FinanceDashboard />} />
         </Routes>
       </BrowserRouter>
     </>
