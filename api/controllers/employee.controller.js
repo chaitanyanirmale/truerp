121479ec615bac43fc95551faadd18b2f7dd39b2
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-17 12:29:40 +0530

    Add Employee controller complete

---
+++ api/controllers/employee.controller.js
@@ -0,0 +1,36 @@
+import Employee from "../models/employee.model.js";
+
+
+export const addEmployee = async (req, res, next) => {
+    try {
+        const {
+            role, fullname, reporting, mobile, email, gender, dob, aadharNumber, panNumber, joiningDate, leavingDate, salaryPerDay, salaryPerHour, pfUanNo, biometricId, department, bankName, bankAccountNo, ifscCode, address, state, location, pinCode
+        } = req.body;
+
+        if (!role || !fullname || !mobile || !email) {
+            return res.status(400).json({
+                success: false,
+                message: "Required fields missing",
+            });
+        }
+        const existingEmployee = await Employee.findOne({ mobile });
+        if (existingEmployee) {
+            return res.status(400).json({
+                success: false,
+                message: "Employee with this mobile already exists",
+            });
+        }
+        const newEmployee = new Employee({
+            role, fullname, reporting, mobile, email, gender, dob, aadharNumber, panNumber, joiningDate, leavingDate, salaryPerDay, salaryPerHour, pfUanNo, biometricId, department, bankName, bankAccountNo, ifscCode, address, state, location, pinCode
+        });
+        const savedEmployee = await newEmployee.save();
+            res.status(201).json({
+            success: true,
+            message: "Employee added successfully",
+            data: savedEmployee,
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
