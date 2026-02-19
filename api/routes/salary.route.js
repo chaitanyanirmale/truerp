4eedc899d21ebf7d529275c07546460d4f03bed7
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-19 11:35:30 +0530

    routes updated

--- api/routes/salary.route.js
+++ api/routes/salary.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import Salary from "../models/salary.model.js";
-import { createSalary } from "../controllers/salary.controller.js";
+import { createSalary, getSalaries } from "../controllers/salary.controller.js";
 
 
 const router = express.Router();
 
 router.post('/create', createSalary);
+router.get('/salary-list', getSalaries);
 
 export default router;
