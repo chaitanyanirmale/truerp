0dd4c99e8c59538c4e05a9942eaf1694e0853995
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-20 21:53:27 +0530

    Create SO controller complete

---
+++ api/controllers/so.controller.js
@@ -0,0 +1,52 @@
+import SO from '../models/so.model.js';
+import User from '../models/user.model.js'
+
+export const createSO = async (req, res, next) => {
+    try {
+        if (!req.user || req.user.role !== "admin") {
+            return res.status(403).json({
+                success: false,
+                message: "Only admin can create Sales Order",
+            });
+        }
+
+        const { customer, soNumber, jobCardNumber, itemDesc, itemQty, majorMinorNumber, receivedDate, expectedDate, status, orderType, drawingRevisionNumber, poNumber, poDate, remark} = req.body;
+        
+        if (!customer || !soNumber || !itemDesc || !itemQty) {
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all required fields",
+            });
+        }
+        const existingCustomer = await User.findOne({
+            _id: customer,
+            role: "customer",
+        });
+
+         if (!existingCustomer) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid customer selected",
+            });
+        }
+
+        const existingSO = await SO.findOne({ soNumber });
+
+        if (existingSO) {
+            return res.status(400).json({
+                success: false,
+                message: "SO Number already exists",
+            });
+        }
+        
+        const newSO = await SO.create({ customer, soNumber, jobCardNumber, itemDesc, itemQty, majorMinorNumber, receivedDate, expectedDate, status, orderType, drawingRevisionNumber, poNumber, poDate, remark});
+
+        res.status(201).json({
+            success: true,
+            message: "Sales Order created successfully",
+            data: newSO,
+        });
+    } catch (error) {
+        next(error)
+    }
+} 
