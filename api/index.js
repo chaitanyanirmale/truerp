942ead64480cc9050db0ce3f3ac23f3a4f59a87e
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-20 21:54:05 +0530

    So routed added to app

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import userRoutes from './routes/user.route.js';
 import expenseRoutes from './routes/expense.route.js';
 import employeeRoutes from "./routes/employee.route.js";
 import salaryRoutes from "./routes/salary.route.js";
+import soRoutes from "./routes/so.route.js";
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -19,7 +20,10 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express();
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 app.use(express.json())
 
 app.use('/api/auth', authRoutes);
@@ -27,6 +31,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/expense', expenseRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/salary", salaryRoutes);
+app.use("/api/so", soRoutes)
 
 app.listen(3000, () => {
   console.log("Api is running on port 3000")
