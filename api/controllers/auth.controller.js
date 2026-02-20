3f44ebd11d670429f850424c19a0f9a6af1bcfef
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-20 21:53:03 +0530

    auth user updated

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,7 +34,7 @@ export const signin = async (req, res) => {
       if (!validPassword) {
          return res.status(400).json({message: "Invalid username or password"});
       }
-      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: validUser._id, role: validUser.role }, process.env.JWT_SECRET);
       const {password: pass, ...user} = validUser._doc;
       res
          .cookie('access_token', token, {httpOnly: true})
