a3fb2f44056c0750f86bff58ab7e0e447d3929b3
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-21 11:56:56 +0530

    getSO route added

--- api/routes/so.route.js
+++ api/routes/so.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createSO } from '../controllers/so.controller.js';
+import { createSO, getSO } from '../controllers/so.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
 router.post('/create',verifyToken, createSO);
+router.get('/so-list', getSO);
 
 export default router;
