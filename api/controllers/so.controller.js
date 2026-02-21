857805bb194a5cc8481568cf98fdfe7f7d1457c0
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-21 11:56:37 +0530

    getso controller added

--- api/controllers/so.controller.js
+++ api/controllers/so.controller.js
@@ -50,3 +50,20 @@ export const createSO = async (req, res, next) => {
         next(error)
     }
 } 
+
+
+export const getSO = async (req, res, next) => {
+  try {
+    const so = await SO.find().populate("customer", "name email role").sort({ createdAt: -1 });
+
+    res.status(200).json({
+      success: true,
+      count: so.length,
+      data: so,
+    });
+
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
