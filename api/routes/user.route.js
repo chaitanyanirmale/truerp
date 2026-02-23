f00c56a98e0174973c6835dde9b027aed26f3d03
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-23 20:41:18 +0530

    update user route added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { getCustomers, getUser, getUsers } from '../controllers/user.controller.js';
+import { getCustomers, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/user', verifyToken, getUser)
 router.get('/user-list', verifyToken, getUsers)
 router.get('/customers', getCustomers)
+router.put('/update/:id', verifyToken, updateUser)
 
 
 
