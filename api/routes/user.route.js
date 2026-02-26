299285f500b57f10e5315d9a7970d17e711239a9
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-02-26 21:47:57 +0530

    suppliers route added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { getCustomers, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
+import { getCustomers, getSuppliers, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/user', verifyToken, getUser)
 router.get('/user-list', verifyToken, getUsers)
 router.get('/customers', getCustomers)
+router.get('/suppliers', getSuppliers)
 router.put('/update/:id', verifyToken, updateUser)
 
 
