1d3354302e974b9504fd591df8e53af5e018e840
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-03 10:19:22 +0530

    route updated

--- api/routes/employee.route.js
+++ api/routes/employee.route.js
@@ -4,8 +4,8 @@ import { addEmployee, getEmployee, getEmployees } from "../controllers/employee.
 const router = express.Router();
 
 
-router.get("/employee", getEmployee);
 router.post("/add-emp", addEmployee);
+router.get("/employee", getEmployee);
 router.get("/emp-list", getEmployees);
 
 export default router;
