2faefc86fcc0457197497aa5aaae7e234db69835
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-03 10:18:40 +0530

    Sub Category controller added

--- api/controllers/category.controller.js
+++ api/controllers/category.controller.js
@@ -41,3 +41,62 @@ export const getCategories = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const addSubCategory = async (req, res, next) => {
+  try{
+    const {mainCategory, subCategory} = req.body;
+    if (!mainCategory || !subCategory) {
+        return res.status(400).json({
+        success: false,
+        message: "Required fields missing",
+      });
+    }
+    const parentCategory = await Category.findById(mainCategory);
+    if (!parentCategory) {
+      return res.status(404).json({
+        success: false,
+        message: "Main category not found",
+      });
+    }
+
+    const lastCategory = await Category.findOne()
+      .sort({ sequenceNumber: -1 });
+
+    const nextSequence = lastCategory ? lastCategory.sequenceNumber + 1 : 1;
+
+    const newSubCategory = new Category({
+      name: subCategory.trim(),
+      parent: mainCategory,
+      sequenceNumber: nextSequence,
+    })
+
+    const savedCategory = await newSubCategory.save();
+    res.status(201).json({
+      success: true,
+      message: "Sub Category added successfully",
+      data: savedCategory,
+    });
+  }catch(error){
+    console.log(error);
+    next(error);
+  }
+}
+
+export const getSubCategories = async (req, res, next) => {
+  try {
+    const subCategories = await Category.find({
+      parent: { $ne: null },
+    })
+      .populate("parent", "name")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      success: true,
+      data: subCategories,
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
