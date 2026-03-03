e463c8c4701d2354621a5c86c3657a99817f2c0f
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-03 10:19:05 +0530

    Sub category route added

--- api/routes/category.route.js
+++ api/routes/category.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { addCategory, getCategories } from '../controllers/category.controller.js';
+import { addCategory, addSubCategory, getCategories, getSubCategories } from '../controllers/category.controller.js';
 
 const router = express.Router();
 
 router.post("/add-category", addCategory)
+router.post("/add-subcategory", addSubCategory)
 router.get("/category-list", getCategories)
+router.get("/subcategory-list", getSubCategories)
 
 
 export default router;
