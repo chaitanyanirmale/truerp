5219b62a82153ba180a78485747528b64962ca1b
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-07 21:14:57 +0530

    route added

--- api/routes/item.route.js
+++ api/routes/item.route.js
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
-import { addItem, previewItemCode } from '../controllers/item.controller.js'
+import { addItem, getItems, previewItemCode } from '../controllers/item.controller.js'
 
 const router =  express.Router()
 
 router.post('/add-item', addItem);
 router.get('/previewItemCode/:prefix', previewItemCode)
+router.get('/list', getItems)
 
 export default router;
