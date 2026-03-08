56a71366a48e80dea3b5182b235d014d47de4290
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-08 09:48:21 +0530

    Supplier Product Schema model Created

---
+++ api/models/supplier.product.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const supplierProductSchema = new mongoose.Schema({
+    suppliers:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    unit: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    stockCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+)
+
+const SupplierProduct = mongoose.model("SupplierProduct", supplierProductSchema)
+
+export default SupplierProduct;
