eb8904833df68f91e5e75ad61ec07e2b89c4a826
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-08 10:05:28 +0530

    route added

--- api/routes/item.route.js
+++ api/routes/item.route.js
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
-import { addItem, getItems, previewItemCode } from '../controllers/item.controller.js'
+import { addItem, addSupplierProduct, getItems, previewItemCode } from '../controllers/item.controller.js'
 
 const router =  express.Router()
 
 router.post('/add-item', addItem);
+router.post('/supplier-product', addSupplierProduct);
 router.get('/previewItemCode/:prefix', previewItemCode)
 router.get('/list', getItems)
 
