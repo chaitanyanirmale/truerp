1bf0ed41e7b2331406a0c5f5a17cf97e72d86e47
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-14 12:03:49 +0530

    route for get invoices added

--- api/routes/invoice.route.js
+++ api/routes/invoice.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createInvoice, previewInvoice } from '../controllers/invoice.controller.js';
+import { createInvoice, getInvoices, previewInvoice } from '../controllers/invoice.controller.js';
 
 const router = express.Router();
 
 router.get('/previewInvoice', previewInvoice);
 router.post('/create', createInvoice);
+router.get('/invoice-list', getInvoices);
 
 export default router;
