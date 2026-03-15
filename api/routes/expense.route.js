c588c9bc1250fe4a54a900e8743144fc868ae7a1
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-15 11:23:18 +0530

    delete expense route added

--- api/routes/expense.route.js
+++ api/routes/expense.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addExpense, getExpenses } from '../controllers/expense.controller.js';
+import { addExpense, deleteExpense, getExpenses } from '../controllers/expense.controller.js';
 
 const router = express.Router();
 
 router.post('/add-expense', addExpense);
 router.get('/get-expenses', getExpenses);
+router.delete('/delete/:id', deleteExpense);
 
 export default router;
