289aa6931b32b3d6be8b99799ee7721afab1b308
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-15 11:23:37 +0530

    delete functionality added

--- client/src/pages/Expanse/ExpenseList.jsx
+++ client/src/pages/Expanse/ExpenseList.jsx
@@ -2,42 +2,79 @@ import React, { useEffect, useState } from 'react'
 
 export const ExpenseList = () => {
   const [expenses, setExpenses] = useState([])
+  const [employees, setEmployees] = useState([])
   const [loading, setLoading] = useState(false)
 
   const fetchExpenses = async () => {
     setLoading(true)
     try {
-            const res = await fetch('/api/expense/get-expenses');
-            const data = await res.json();
-            if(data.success === true){
-                setExpenses(data.data)
-            }
-            setLoading(false);
-        } catch (error) {
-            console.log(error)
-            setLoading(false);
-        }
+      const res = await fetch('/api/expense/get-expenses');
+      const data = await res.json();
+      if(data.success === true){
+          setExpenses(data.data)
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   }
-
+  const fetchEmployees = async () => {
+      setLoading(true);
+      try {
+          const res = await fetch('/api/employee/emp-list');
+          const data = await res.json();
+          if(data.success === true){
+            setEmployees(data.data)
+          }
+          setLoading(false);
+      } catch (error) {
+          console.log(error)
+          setLoading(false);
+      }
+  }
+    
   useEffect(()=> {
+    fetchEmployees();
     fetchExpenses();
   },[])
+
+  const deleteExpense = async (id) => {
+        try {
+            const res = await fetch(`/api/expense/delete/${id}`, {
+            method: "DELETE",
+            });
+            const data = await res.json();
+            if (data.success) {
+            alert("Expense deleted successfully");
+
+            setExpenses(expenses.filter((expense) => expense._id !== id));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
   const totalExpense = expenses.reduce(
     (total, exp) => total + exp.amount, 0
   );
   return (
     <div>
-    <div className='p-2 bg-white border border-slate-400 shadow-md rounded-sm'>
+    <div className='p-2 bg-white shadow-md rounded-sm'>
       <h4 className='mb-4 text-2xl font-semibold'>List of Expenses</h4>
       <hr className='text-slate-600'/>
       <form action="">
-        <div className="m-2">
+        <div className="m-2 text-sm">
           <div className="flex justify-between bg-gray-200 px-5 py-2 ">
-              <input type="text" name="" id="" placeholder='From Date' className="border border-slate-400 rounded-sm p-3 py-1 bg-white"/>
-              <input type="text" name="" id="" placeholder='To Date' className="border border-slate-400 rounded-sm p-3 py-1 bg-white"/>
-               <select type="text" name="" id="" className="border border-slate-400 rounded-sm p-3 py-1 bg-white">
+              <input type="text" name="" placeholder='From Date' className="border border-slate-400 rounded-sm p-3 py-1 bg-white"/>
+              <input type="text" name="" placeholder='To Date' className="border border-slate-400 rounded-sm p-3 py-1 bg-white"/>
+               <select type="text" name="" className="border border-slate-400 rounded-sm p-3 py-1 bg-white">
                 <option value="">---Select Employee---</option>
+               {employees.map((emp) => (
+                  <option key={emp._id} value={emp._id}>
+                  {emp.fullname}
+                  </option>
+                ))}
               </select>
             <div className="">
               <button type='submit' name='submit' className='bg-blue-600 text-white p-2 rounded-sm'><i className='fa fa-fw fa-search'></i>Search</button>
@@ -72,7 +109,9 @@ export const ExpenseList = () => {
                 <td className='p-2 border border-slate-400'>{exp.note}</td>
                 <td className='p-2 border border-slate-400'>{exp.employee?.fullname}</td>
                 {/* <td className='p-2 border border-slate-400'><a href="" className='bg-yellow-500 px-2 py-1 rounded-sm '><i className="fa fa-plus px-1" aria-hidden="true"></i>Attach file</a></td> */}
-                <td className='p-2 border border-slate-400'><a href="" className='bg-rose-600 px-2 py-1 rounded-sm text-white'><i className="fa fa-trash px-2" aria-hidden="true"></i>Delete</a></td>
+                <td className='p-2 border border-slate-400'>
+                  <button onClick={() => deleteExpense(exp._id)} className='bg-rose-600 p-2 rounded-sm text-white hover:bg-rose-700'><i className="fa fa-trash px-2"></i>Delete</button>
+                </td>
               </tr>)
               }             
             </tbody>
