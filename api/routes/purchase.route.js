fdd45a03bd72500df1be91ad3898c116550c40e6
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-17 12:22:14 +0530

    Delete Purchase Route added

--- api/routes/purchase.route.js
+++ api/routes/purchase.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { addPurchase, getPurchaseList } from '../controllers/purchase.controller.js';
+import { addPurchase, deletePurchase, getPurchaseList } from '../controllers/purchase.controller.js';
 
 
 const router = express.Router();
 
 router.post("/add-purchase", verifyToken, addPurchase);
 router.get('/purchase-list', getPurchaseList);
+router.delete('/delete/:id', deletePurchase)
 
 export default router;
