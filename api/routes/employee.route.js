c92ad1c959fb5d003115307a653679919f6a3637
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-18 12:14:46 +0530

    generate emp code route added

--- api/routes/employee.route.js
+++ api/routes/employee.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addEmployee, getEmployee, getEmployees } from "../controllers/employee.controller.js";
+import { addEmployee, getEmployee, getEmployeeCode, getEmployees } from "../controllers/employee.controller.js";
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post("/add-emp", addEmployee);
 router.get("/employee", getEmployee);
 router.get("/emp-list", getEmployees);
+router.get("/generate-code", getEmployeeCode);
+
 
 export default router;
