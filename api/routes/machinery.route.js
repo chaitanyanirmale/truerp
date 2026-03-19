e2e2512c7a55b525676c481c38404b359bc920a7
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-19 22:55:06 +0530

    get machinery route added

--- api/routes/machinery.route.js
+++ api/routes/machinery.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { addMachinery } from '../controllers/machinery.controller.js';
+import { addMachinery, getMachineryList } from '../controllers/machinery.controller.js';
 
 const router = express.Router();
 
 router.post('/add-machinery', addMachinery);
+router.get('/machinery-list', getMachineryList);
 
 export default router;
