9344b5747550bed24f685c02293c27d5dbb9b8a4
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-21 17:34:31 +0530

    Added generate po number controller and preview po controller

---
+++ api/controllers/po.controller.js
@@ -0,0 +1,61 @@
+import Counter from '../utils/counter.js'
+export const getFinancialYear = () => {
+
+  const now = new Date();
+  const month = now.getMonth() + 1; 
+  const year = now.getFullYear();
+
+  let startYear;
+  let endYear;
+
+  if (month < 4) {
+    startYear = year - 1;
+    endYear = year;
+  } else {
+    startYear = year;
+    endYear = year + 1;
+  }
+
+  return `${startYear}-${String(endYear).slice(-2)}`;
+};
+
+
+export const generatePoNumber = async () => {
+    const counter = await Counter.findOneAndUpdate(
+        {name: "po"},
+        {$inc: {sequence: 1}},
+        {returnDocument: "after", upsert: true}
+    );
+
+    const sequence = counter.sequence;
+    const poNumber = String(sequence).padStart(4, "0");
+    const fy = getFinancialYear();
+
+    return `VEL-${poNumber}-${fy}`;
+}
+
+
+export const previewPoNumber = async (req, res, next) => {
+    try {
+            let counter = await Counter.findOne({ name: "po"});
+            if(!counter){
+                counter = await Counter.create({
+                    name:"po",
+                    sequence: 0,
+                })
+            }
+        
+            const nextSequence = (counter.sequence || 0) + 1;
+            const padded = String(nextSequence).padStart(4, "0");
+            const fy = getFinancialYear();
+        
+            const poNumber = `VEL-${padded}-${fy}`;
+        
+            res.status(200).json({
+                success: true,
+                poNumber
+            });
+    } catch (error) {
+        next(error)
+    }
+}
