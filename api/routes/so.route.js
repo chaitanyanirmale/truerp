b4953774ce19b187b0fa735d2122383a41eb89a5
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-27 21:50:58 +0530

    preview job card number route added

--- api/routes/so.route.js
+++ api/routes/so.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createSO, getSO } from '../controllers/so.controller.js';
+import { createSO, getSO, previewJCNumber } from '../controllers/so.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
 router.post('/create',verifyToken, createSO);
 router.get('/so-list', getSO);
+router.get('/previewJCNumber', previewJCNumber);
 
 export default router;
