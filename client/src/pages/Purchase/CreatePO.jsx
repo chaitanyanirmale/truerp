40f5c7bc75f758efce4b9ec225b9acf0598f2417
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-27 11:00:42 +0530

    Create Po Complete with functionality

--- client/src/pages/Purchase/CreatePO.jsx
+++ client/src/pages/Purchase/CreatePO.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
 export const CreatePO = () => {
-  const initialData = { poNo:"", poDate:"" }
+  const initialData = {so: '', supplier: '', poNo:"", poDate:"" }
   const [formData, setFormData] = useState(initialData);
   const [sos, setSO] = useState([]);
+  const [suppliers, setSuppliers] = useState([]);
   const [loading, setLoading] = useState(false);
   const fetchPoNo = async () => {
         const res = await fetch("/api/po/previewPoNumber");
@@ -15,7 +16,7 @@ export const CreatePO = () => {
                 poNo : data.poNumber
             }))
         }
-    }
+  }
   const setTodayDate = () => {
     const today = new Date().toISOString().split("T")[0];
           
@@ -24,34 +25,82 @@ export const CreatePO = () => {
         poDate:today
     }))
   }
-    const fetchSO = async () => {
-      setLoading(true);
+  const fetchSO = async () => {
       try {
           const res = await fetch('/api/so/so-list');
           const data = await res.json();
           if(data.success === true){
             setSO(data.data)
           }
-          setLoading(false);
       } catch (error) {
         console.log(error)
-        setLoading(false);
       }
+  }
+  const fetchSuppliers = async () => {
+    try {
+        const res = await fetch('/api/users/suppliers');
+        const data = await res.json();
+        if(data.success === true){
+            setSuppliers(data.data)
+        }
+    } catch (error) {
+        console.log(error)
     }
+  }
   useEffect(() => {
     fetchPoNo();
     setTodayDate();
     fetchSO();
+    fetchSuppliers();
   }, []);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData, 
+      [e.target.name] : e.target.value 
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+    try {
+        const res = await fetch("/api/po/createPo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials:"include",
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(!res.ok){
+        console.log("server error")
+        return;
+      }
+      console.log("Po Created Successfully")
+      setFormData(initialData)
+      fetchPoNo();
+      setTodayDate();
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className='bg-white rounded-sm shadow p-4 w-1/2'>
       <h1 className='text-2xl font-semibold'>Create PO</h1>
       <hr className='text-slate-300 my-4'/>
-      <div className="mb-6">
-        <select name="so" className="border border-slate-300 p-2 rounded-sm w-full">
+      <form onSubmit={handleSubmit}>
+      <div className="mb-6 grid gap-4">
+        <select name="so" onChange={handleChange} value={formData.so} className="border border-slate-300 p-2 rounded-sm w-full">
           <option value="">-- Select SO --</option>
           {sos.map((so)=> (
             <option key={so._id} value={so._id}>{so.soNumber}-{so.itemDesc}</option>
+          ))}
+        </select>
+        <select name="supplier" onChange={handleChange} className="border border-slate-300 p-2 rounded-sm w-full">
+          <option value="">-- Select Supplier --</option>
+          {suppliers.map((supplier)=> (
+            <option key={supplier._id} value={supplier._id}>{supplier.companyName}</option>
           ))}
         </select>
       </div>
@@ -62,19 +111,20 @@ export const CreatePO = () => {
         </div>
         <div className="grid gap-2">
           <label className="text-sm font-semibold">PO Date</label>
-          <input type="date" name="pdDate" value={formData.poDate} className="border border-slate-300 p-2 rounded-sm" />
+          <input type="date" name="poDate" onChange={handleChange} value={formData.poDate} className="border border-slate-300 p-2 rounded-sm" />
         </div>
       </div>
       <div className="grid gap-2 mt-2">
         <label className="text-sm font-semibold">PO Terms & Conditions</label>
-        <textarea name="terms&Conditions" className="border border-slate-300 p-2 rounded-sm text-sm text-gray-700" rows="2" 
+        <textarea className="border border-slate-300 p-2 rounded-sm text-sm text-gray-700" rows="2" 
         defaultValue={`1. Terms of Payment : 60 Days Credit \n2. Delivery : Immediately  within 1-2 Weeks \n3. Freights : At Actual \n4. Packing And Forwarding : NIL \n5. Other Reference :  \n\nYou are Requested to acknowledge the Acceptance of this Order to us Immediately. If you fail to acknowledge the acceptance within 2 days the we will assume that you have accepted this order.`}>         
         </textarea>
       </div>
       <hr className='text-slate-300 my-4'/>
       <div className="">
-        <button className='bg-blue-700 hover:bg-blue-800 p-2 px-4 text-white font-semibold rounded-xs'><i className='fa fa-check-circle pr-1'></i>Submit</button>
+        <button type='submit' className='bg-blue-700 hover:bg-blue-800 p-2 px-4 text-white font-semibold rounded-xs'><i className='fa fa-check-circle pr-1'></i>Submit</button>
       </div>
+    </form>
     </div>
   )
 }
