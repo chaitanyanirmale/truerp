f989422d083d5889aca5d9a05bd59393cdb6fa6f
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-27 10:59:31 +0530

    Po Model Created

--- api/models/po.model.js
+++ api/models/po.model.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 
 const poSchema = mongoose.Schema({
+    so: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SO",
+        required: true
+    },
+    supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
     poNo:{
         type: String,
         required: true,
