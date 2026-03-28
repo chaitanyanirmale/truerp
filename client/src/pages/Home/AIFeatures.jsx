d545fbce7137a6bf92b035788f4a79d4a849023e
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-28 13:45:40 +0530

    AI feature page ui updated with images

--- client/src/pages/Home/AIFeatures.jsx
+++ client/src/pages/Home/AIFeatures.jsx
@@ -8,134 +8,74 @@ export const AIFeatures = () => {
             <p className='text-sm text-gray-600'>Explore intelligent automation across all ERP modules</p>
         </div>        
         <div className="mt-4 p-2 gap-10 grid grid-cols-3">
-            <div className="bg-white w-full rounded-md">
+            <div className="bg-white w-full rounded-md shadow-lg">
                 <div className="p-6">
                     <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-line-chart'></i></div>                   
                     <h5 className='text-lg font-semibold'>AI Demand Forecasting</h5>
                     <p className='text-sm my-2'>Predicts future demand accurately.</p>
+                    <img src="\ai_demand.jpg" alt="" className='h-50'/>
                     <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
+                        <button className="bg-blue-600 px-2 py-1 rounded-xs text-white text-sm font-semibold  items-end hover:shadow-lg" id="viewbutton"
                         >View Details</button>
                     </div>
                 </div>
             </div>
-            <div className="bg-white w-full rounded-md">
-                <div className="p-6">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-gears'></i></div>                   
-                    <h5 className='text-lg font-semibold'>Predictive Machine Maintenance</h5>
-                    <p className='text-sm my-2'>Avoids unexpected breakdowns.</p>
-                    <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
-                        >View Details</button>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-white w-full rounded-md">
+            <div className="bg-white w-full rounded-md shadow-lg">
                 <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-check-circle'></i></div>                   
-                    <h5 className='text-lg font-semibold'>AI Quality Control</h5>
-                    <p className='text-sm my-2'>Improves product quality.</p>
-                    <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
-                        >View Details</button>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-white w-full rounded-md">
-                <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-boxes-stacked'></i></div>                   
+                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i class="fa fa-boxes-stacked"></i></div>                   
                     <h5 className='text-lg font-semibold'>Smart Inventory Optimization</h5>
                     <p className='text-sm my-2'>Optimizes stock levels.</p>
+                    <img src="\ai_smart_inventory.jpg" alt="" className='h-50'/>
                     <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
+                        <button className="bg-blue-600 px-2 py-1 rounded-xs text-white text-sm font-semibold  items-end hover:shadow-lg" id="viewbutton"
                         >View Details</button>
                     </div>
                 </div>
             </div>
-            <div className="bg-white w-full rounded-md">
+            <div className="bg-white w-full rounded-md shadow-lg">
                 <div className="p-6 ">
                     <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-robot'></i></div>                   
                     <h5 className='text-lg font-semibold'>AI ERP Assistant</h5>
                     <p className='text-sm my-2'>Chat with your ERP.</p>
+                    <img src="\ai_assistant.jpg" alt="" className='h-50'/>
                     <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
+                        <button className="bg-blue-600 px-2 py-1 rounded-xs text-white text-sm font-semibold  items-end hover:shadow-lg" id="viewbutton"
                         >View Details</button>
                     </div>
                 </div>
             </div>
-            <div className="bg-white w-full rounded-md">
+            <div className="bg-white w-full rounded-md shadow-lg">
                 <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-indian-rupee-sign'></i></div>                   
+                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-inr'></i></div>                   
                     <h5 className='text-lg font-semibold'>AI Costing & Profit Prediction</h5>
                     <p className='text-sm my-2'>Predicts job profitability.</p>
+                    <img src="\ai_costing.jpg" alt="" className='h-50'/>
                     <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
+                        <button className="bg-blue-600 px-2 py-1 rounded-xs text-white text-sm font-semibold  items-end hover:shadow-lg" id="viewbutton"
                         >View Details</button>
                     </div>
                 </div>
             </div>
-            <div className="bg-white w-full rounded-md">
+            <div className="bg-white w-full rounded-md shadow-lg">
                 <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-file-shield'></i></div>                   
+                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-file'></i></div>                   
                     <h5 className='text-lg font-semibold'>AI Compliance Monitoring</h5>
                     <p className='text-sm my-2'>Ensures regulatory compliance.</p>
+                    <img src="\ai_compliance.jpg" alt="" className='h-50'/>
                     <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
+                        <button className="bg-blue-600 px-2 py-1 rounded-xs text-white text-sm font-semibold  items-end hover:shadow-lg" id="viewbutton"
                         >View Details</button>
                     </div>
                 </div>
             </div>
-            <div className="bg-white w-full rounded-md">
-                <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-user-gear'></i></div>                   
-                    <h5 className='text-lg font-semibold'>AI Workforce Optimization</h5>
-                    <p className='text-sm my-2'>Improves manpower efficiency.</p>
-                    <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
-                        >View Details</button>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-white w-full rounded-md">
-                <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-truck-fast'></i></div>                   
-                    <h5 className='text-lg font-semibold'>Smart Dispatch & Delivery AI</h5>
-                    <p className='text-sm my-2'>Optimizes deliveries.</p>
-                    <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
-                        >View Details</button>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-white w-full rounded-md">
-                <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-magnifying-glass-chart'></i></div>                   
-                    <h5 className='text-lg font-semibold'>AI Root Cause Analysis</h5>
-                    <p className='text-sm my-2'>Finds hidden problems.</p>
-                    <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
-                        >View Details</button>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-white w-full rounded-md">
+            <div className="bg-white w-full rounded-md shadow-lg">
                 <div className="p-6 ">
                     <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-handshake-angle'></i></div>                   
                     <h5 className='text-lg font-semibold'>AI Supplier Intelligence</h5>
                     <p className='text-sm my-2'>Evaluates supplier risks.</p>
+                    <img src="\ai_supplier_int.jpg" alt="" className='h-50'/>
                     <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
-                        >View Details</button>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-white w-full rounded-md">
-                <div className="p-6 ">
-                    <div className="w-20 mb-2 text-blue-600 text-4xl"><i className='fa fa-bolt'></i></div>                   
-                    <h5 className='text-lg font-semibold'>AI Energy Optimization</h5>
-                    <p className='text-sm my-2'>Reduces energy costs.</p>
-                    <div className="flex justify-end w-full">
-                        <button className="bg-blue-600 px-2 py-1 rounded-md text-white text-sm font-semibold  items-end" id="viewbutton"
+                        <button className="bg-blue-600 px-2 py-1 rounded-xs text-white text-sm font-semibold  items-end hover:shadow-lg" id="viewbutton"
                         >View Details</button>
                     </div>
                 </div>
