00ec1c86eb09c4c7dd2cebaec3df94cc4fea0fa9
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-29 20:45:08 +0530

    getPo route added

--- api/routes/po.route.js
+++ api/routes/po.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createPo, previewPoNumber } from '../controllers/po.controller.js';
+import { createPo, getPO, previewPoNumber } from '../controllers/po.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
 router.get('/previewPoNumber', previewPoNumber);
-router.post('/createPo',verifyToken, createPo)
+router.post('/createPo',verifyToken, createPo);
+router.get('/po-list', getPO);
 
 export default router;
