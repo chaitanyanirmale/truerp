f31fea03394da6b6acefbff5374bafd358db1754
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-30 14:19:13 +0530

    route for get enquiries added

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { adduser, signin } from "../controllers/auth.controller.js";
-import { saveContactMessage } from "../controllers/contact.controller.js";
 
 const router = express.Router();
 
 router.post("/adduser", adduser);
 router.post("/signin", signin);
-router.post("/send-message", saveContactMessage);
 
 export default router;
