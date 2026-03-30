c6c61abd31e817683315e4f8403caed970b55a94
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-30 10:48:41 +0530

    delete functionality added

--- client/src/pages/Salary/EmployeeList.jsx
+++ client/src/pages/Salary/EmployeeList.jsx
@@ -5,7 +5,10 @@ import { useState } from 'react'
 export const EmployeeList = () => {
     const [employees, setEmployees] = useState([]);
     const [loading, setLoading] = useState(false)
-
+    const [openDropdown, setOpenDropdown] = useState(null);
+    const toggleDropdown = (id) => {
+        setOpenDropdown(openDropdown === id ? null : id);
+    };
     const fetchEmployees = async () => {
         setLoading(true);
         try {
@@ -23,12 +26,34 @@ export const EmployeeList = () => {
     useEffect(() => {
         fetchEmployees();
     }, []);
+
+    const handleDelete = async (id) => {
+        try {
+            const res = await fetch(`/api/employee/delete-emp/${id}`, {
+                method: "DELETE",
+                credentials: "include"
+            });
+
+            const data = await res.json();
+
+            if (data.success) {
+            alert("Employee deleted successfully");
+
+            setEmployees((prev) =>
+                prev.filter((emp) => emp._id !== id)
+            );
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
   return (
 
     <div className='p-4 bg-white border border-slate-200 rounded-sm'>
         <div className="flex justify-between">
             <h1 className='text-2xl font-semibold'>List of Employee</h1>
-            <select name="" id="" className='p-2 border border-slate-400 rounded-sm'>
+            <select name="" className='p-2 border border-slate-400 rounded-sm'>
                 <option value="all-emp">All Employees</option>
                 <option value="staff">Staff</option>
                 <option value="worker">Worker</option>
@@ -37,7 +62,7 @@ export const EmployeeList = () => {
         <hr className='text-slate-400 my-4'/>
         {loading ? (
             <p>Loading...</p>
-        ): <div className="text-center">
+        ): <div className="text-center overflow-y-auto">
             <table className='w-full text-sm'>
                 <thead className='text-gray-700 uppercase text-xs'>
                     <tr>
@@ -72,9 +97,22 @@ export const EmployeeList = () => {
                             </span>
                             </td>
                             <td className="px-6 py-4 border text-center">
-                            <button className="px-3 py-2 text-sm text-white bg-blue-600 rounded hover:bg-blue-700 transition">
+                            <button onClick={()=> toggleDropdown(emp._id)} className="px-3 py-2 text-sm text-white bg-blue-600 rounded-xs hover:bg-blue-700 transition">
                                 Actions
+                                <i className='fa fa-angle-down pl-1'></i>
                             </button>
+                            {openDropdown === emp._id && (
+                                <div className="absolute right-20 w-50 bg-white shadow-sm border border-slate-300 z-10">
+                                    <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                                    >View</button>
+                                    <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                                    >Activate</button>
+                                    <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                                    >Deactivate</button>
+                                    <button onClick={()=> handleDelete(emp._id)} className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                                    >Delete</button>
+                                </div>
+                            )}
                         </td>
                     </tr>
                     ))                    
