7504edf6d973b5afc07f4885d49cde1a1a648b85
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-30 10:48:23 +0530

    delete employee route added

--- api/routes/employee.route.js
+++ api/routes/employee.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addEmployee, getEmployee, getEmployeeCode, getEmployees } from "../controllers/employee.controller.js";
+import { addEmployee, deleteEmp, getEmployee, getEmployeeCode, getEmployees } from "../controllers/employee.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.post("/add-emp", addEmployee);
 router.get("/employee", getEmployee);
 router.get("/emp-list", getEmployees);
 router.get("/generate-code", getEmployeeCode);
+router.delete('/delete-emp/:id', deleteEmp);
 
 
 export default router;
