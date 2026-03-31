0a5a96afe26adc22bdc7a94a8b82a4d6e7ad158c
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-03-31 21:58:27 +0530

    Preview SONumber route added

--- api/routes/so.route.js
+++ api/routes/so.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createSO, getSO, previewJCNumber } from '../controllers/so.controller.js';
+import { createSO, getSO, previewJCNumber, previewSONumber } from '../controllers/so.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post('/create',verifyToken, createSO);
 router.get('/so-list', getSO);
 router.get('/previewJCNumber', previewJCNumber);
+router.get('/previewSONumber', previewSONumber);
 
 export default router;
