27bda41ead56d026c3d4dc2d08e3d4189332fc01
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-01 19:19:57 +0530

    update purchase bill route added

--- api/routes/purchase.route.js
+++ api/routes/purchase.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { addPurchase, deletePurchase, getPurchaseList, purchaseBill } from '../controllers/purchase.controller.js';
+import { addPurchase, deletePurchase, getPurchaseList, purchaseBill, updatePurchaseBill } from '../controllers/purchase.controller.js';
 
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post("/add-purchase", verifyToken, addPurchase);
 router.get('/purchase-list', getPurchaseList);
 router.delete('/delete/:id', deletePurchase);
 router.get('/purchase-bill/:id', purchaseBill);
+router.put('/update-purchase/:id', updatePurchaseBill);
 
 export default router;
