da71abbfe07943eeda35e51d7f02302e31880d10
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-02 11:46:46 +0530

    get user by id controller added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -15,6 +15,29 @@ export const getUser = async (req, res, next) => {
   }
 }
 
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+            .select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 export const getUsers = async (req, res, next) => {
   try {
     const users = await User.find().sort({ createdAt: -1 });
