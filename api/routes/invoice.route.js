7fc8139d67a2c71e1c2c21db1ebce47dae1f76c7
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-07 18:23:16 +0530

    route for bill number controller added

--- api/routes/invoice.route.js
+++ api/routes/invoice.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createInvoice, getInvoices, previewInvoice } from '../controllers/invoice.controller.js';
+import { createInvoice, getInvoices, getSingleInvoice, previewBillNumber, previewInvoice } from '../controllers/invoice.controller.js';
 
 const router = express.Router();
 
 router.post('/previewInvoice', previewInvoice);
+router.post('/previewBillNo', previewBillNumber);
 router.post('/create', createInvoice);
 router.get('/invoice-list', getInvoices);
+router.get("/:id", getSingleInvoice);
 
 export default router;
