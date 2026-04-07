451059311bf91c02c910dcf4abe9b557e4046bb2
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-07 18:24:32 +0530

    Bills of supply page ready with data fetch

--- client/src/pages/BillsOfSupply.jsx
+++ client/src/pages/BillsOfSupply.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import {data, useNavigate, useParams} from 'react-router-dom';
 
 export const BillsOfSupply = () => {
-    const navigate = useNavigate() 
+    const {id} = useParams();
+    const [invoice, setInvoice] = useState(null);
+
+    const fetchInvoice = async () => {
+        try {
+            
+            const res = await fetch(`/api/invoice/${id}`, {
+                credentials: "include"
+            });
+
+            const data = await res.json();
+
+            setInvoice(data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(()=> {
+        fetchInvoice();
+    },[])
+    const navigate = useNavigate();
   return (
     <div className='p-4 bg-gray-100'>
-        <div className="bg-white shadow-sm rounded-xs px-4 py-2">
+        <div id='bill-content' className="bg-white shadow-sm rounded-xs px-4 py-2">
             <div className="flex justify-between">
                 <img src="\profile.jpg" alt="" className='h-40'/>
                 <div className="text-end pt-8">
@@ -15,29 +35,50 @@ export const BillsOfSupply = () => {
                     <p className='text-gray-600 text-sm'>GSTN: 27AATFV7268A1ZO | PAN: AATFV7268A</p>   
                 </div>
             </div>
+            {invoice && 
             <div className="">
                 <div className="flex justify-between">
                     <table className='w-full'>
                         <tbody>
                             <tr>
                                 <td className='border border-b-0 border-slate-400'>
-                                    <p className='text-2xl text-center font-semibold pt-4'>Bills of Supply</p>
+                                    <p className='text-2xl text-center font-semibold pt-4'>{invoice?.invoiceType}</p>
                                 </td>
                                 <td className='w-1/6 p-2 border border-b-0 border-slate-400'>
-                                    <tr>
-                                        <td className='border border-slate-400'>
-                                            <i className='fa fa-check p-2'></i>
-                                        </td>
-                                        <td className='border border-slate-400 px-2'>Original for Recipient</td>
-                                    </tr>
-                                    <tr>
-                                        <td className='border border-slate-400'></td>
-                                        <td className='border border-slate-400 px-2'>Duplicate for Transporter</td>
-                                    </tr>
-                                    <tr>
-                                        <td className='border border-slate-400'></td>
-                                        <td className='border border-slate-400 px-2'>Triplicate for Supplier</td>
-                                    </tr>
+                                    <table className='w-full border-collapse'>
+                                        <tbody>
+                                            <tr>
+                                                <td className='border border-slate-400'>
+                                                    {invoice?.originalForRecipient && (
+                                                        <i className='fa fa-check p-2'></i>
+                                                    )}
+                                                </td>
+                                                <td className='border border-slate-400 px-2'>
+                                                    Original for Recipient
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className='border border-slate-400'>
+                                                    {invoice?.duplicateForTransporter && (
+                                                        <i className='fa fa-check p-2'></i>
+                                                    )}
+                                                </td>
+                                                <td className='border border-slate-400 px-2'>
+                                                    Duplicate for Transporter
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td className='border border-slate-400'>
+                                                    {invoice?.triplicateForSupplier && (
+                                                        <i className='fa fa-check p-2'></i>
+                                                    )}
+                                                </td>
+                                                <td className='border border-slate-400 px-2'>
+                                                    Triplicate for Supplier
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </td>
                             </tr>
                         </tbody>
@@ -49,21 +90,21 @@ export const BillsOfSupply = () => {
                             <tr>
                                 <td className='border border-b-0 border-slate-400 px-2'>
                                     <p>Reverse No: No</p>
-                                    <p>Invoice No: INL-0001-2026-27</p>
-                                    <p>Invoice Date: 04-04-2026</p>
+                                    <p>Invoice No: {invoice?.invoiceNumber}</p>
+                                    <p>Invoice Date: {new Date(invoice?.invoiceDate).toISOString().split('T')[0]}</p>
                                     <p>State: Maharashtra</p>
                                 </td>
                                 <td className='border border-b-0 border-slate-400 px-2'>
-                                    <p>Transportation Mode: </p>
-                                <p>Vehicle No:</p>
-                                <p>Date of Supply: 04-04-2026</p>
-                                <p>Place of Supply</p>
+                                    <p>Transportation Mode: {invoice?.transportType}</p>
+                                    <p>Vehicle No: {invoice?.vehicleNumber}</p>
+                                    <p>Date of Supply: {new Date(invoice?.dateOfSupply).toISOString().split('T')[0]}</p>
+                                    <p>Place of Supply: {invoice?.placeOfSupply}</p>
                                 </td>
-                                <td className='border border-b-0 border-slate-400 px-2'>
-                                    <p>Challan No: No</p>
-                                    <p>Transporter Name</p>
-                                    <p>PO NO: </p>
-                                    <p>PO Date:</p>
+                                <td className='border border-b-0 border-slate-400 px-2 w-1/5'>
+                                    <p>Challan No: {invoice?.challanNumber}</p>
+                                    <p>Transporter Name: {invoice?.transporterName}</p>
+                                    <p>PO NO: {invoice?.poNumber}</p>
+                                    <p>PO Date: {new Date(invoice?.poDate).toISOString().split('T')[0]}</p>
                                 </td>
                             </tr>
                         </tbody>
@@ -74,22 +115,22 @@ export const BillsOfSupply = () => {
                         <thead>
                             <tr>
                                 <th className='border border-slate-400 p-2'>Details of Receiver | Billed to:</th>
-                                <th className='border border-slate-400 p-2'>Details of Receiver | Billed to:</th>
+                                <th className='border border-slate-400 p-2'>Details of Consignee | Billed to:</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td className='border border-slate-400 px-2'>
-                                    <p>Name</p>
-                                    <p>Address</p>
+                                    <p>Name: {invoice?.receiver?.name}</p>
+                                    <p>Address: {invoice?.receiver?.address}</p>
                                     <p>GSTIN</p>
-                                    <p>State</p>
+                                    <p>State: {invoice?.receiver?.state}</p>
                                 </td>
                                 <td className='border border-slate-400 px-2'>
-                                    <p>Name</p>
-                                    <p>Address</p>
+                                    <p>Name: {invoice?.consignee?.name}</p>
+                                    <p>Address: {invoice?.consignee?.address}</p>
                                     <p>GSTIN</p>
-                                    <p>State</p>
+                                    <p>State: {invoice?.consignee?.state}</p>
                                 </td>                     
                             </tr>
                         </tbody>
@@ -106,56 +147,27 @@ export const BillsOfSupply = () => {
                                 <th className='border border-t-0 border-slate-400'>Unit</th>
                                 <th className='border border-t-0 border-slate-400'>Rate</th>
                                 <th className='border border-t-0 border-slate-400'>Taxable Value</th>
-                                <th className='border border-t-0 border-slate-400 w-30 px-2'>
-                                    <tr className=''>
-                                        <td className='text-center'>CGST</td>
-                                    </tr>
-                                    <tr className='flex justify-between'>
-                                        <td>Rate</td>
-                                        <td>Amount</td>
-                                    </tr>
-                                </th>
-                                <th className='border border-t-0 border-slate-400 w-30 px-2'>
-                                    <tr className=''>SGST</tr>
-                                    <tr className='flex justify-between'>
-                                        <td>Rate</td>
-                                        <td>Amount</td>
-                                    </tr>
-                                </th>
+                                <th colspan="2" className='border-r border-b border-slate-400'><span >CGST</span><hr className='text-slate-400'/>
+                                <small className='pr-4'>Rate</small><small>Amount(<i className="fa fa-inr"></i>)</small></th> 
+                                <th colspan="2" className='border-r border-b border-slate-400' ><span>SGST</span><hr className='text-slate-400'/>
+                                <small  className='pr-4'>Rate</small><small>Amount(<i class="fa fa-inr"></i>)</small></th> 
                                 <th className='border border-t-0 border-slate-400'>Total</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td className='border border-slate-400'>1</td>
-                                <td className='border border-slate-400'></td>
-                                <td className='border border-slate-400'></td>
-                                <td className='border border-slate-400'></td>
-                                <td className='border border-slate-400'>NOS</td>
-                                <td className='border border-slate-400'></td>
-                                <td className='border border-slate-400'></td>
-                                <td className='border border-slate-400'>
-                                    <tr className='flex justify-between'>
-                                        <td className='border-r border-slate-400 w-full'>0%</td>
-                                        <td className=' w-full'>0</td>
-                                    </tr>
-                                </td>
-                                <td className='border border-slate-400'>
-                                    <tr className='flex'>
-                                        <td className='border-r border-slate-400 w-full'>0%</td>
-                                        <td className='w-full'>0</td>
-                                    </tr>
-                                </td>
-                                <td className='border border-slate-400'>0</td>
-                            </tr>
-                            <tr className=''>
-                                <td className='border border-slate-400' colSpan="3">Total</td>
-                                <td className='border border-slate-400'>0</td>
-                                <td className='border border-slate-400' colSpan="2"></td>
-                                <td className='border border-slate-400'>0</td>
-                                <td className='border border-slate-400'>0</td>
-                                <td className='border border-slate-400'>0</td>
-                                <td className='border border-slate-400'>0</td>
+                                <td className='border border-slate-400'> {invoice?.productName}</td>
+                                <td className='border border-slate-400'> {invoice?.hsn}</td>
+                                <td className='border border-slate-400'> {invoice?.quantity}</td>
+                                <td className='border border-slate-400'> {invoice?.unit}</td>
+                                <td className='border border-slate-400'>{invoice?.unitPrice}</td>
+                                <td className='border border-slate-400'>{invoice?.taxableValue}</td>                  
+                                <td className='border border-slate-400'><p>{invoice?.sgstRate}%</p></td> 
+                                <td className='border border-slate-400'>{invoice?.sgstAmount}</td> 
+                                <td className='border border-slate-400'><p>{invoice?.cgstRate}%</p></td>      
+                                <td className='border border-slate-400'>{invoice?.cgstAmount}</td>
+                                <td className='border border-slate-400'>{invoice?.subTotal}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -164,10 +176,10 @@ export const BillsOfSupply = () => {
                     <table className='w-full'>
                         <tbody>
                             <tr>
-                                <td className='border border-slate-400 border-t-0 px-2'>Total Amount in Words:</td>
-                                <td className='border border-t-0 border-slate-400 p-2 w-40'>
-                                    <tr><td className='border-b border-slate-400'>Total GST: 0</td></tr>
-                                    <tr><td className=''>Amount with Tax: 0</td></tr>
+                                <td className='border border-slate-400 border-t-0 px-2' colSpan="12">Total Amount in Words: {invoice?.amountInWords}</td>
+                                <td className='border border-t-0 border-slate-400 p-2 w-20' colSpan="4">
+                                    Total GST: <span><i class="fa fa-inr" ></i> {(invoice?.cgstAmount || 0) + (invoice?.sgstAmount || 0)}</span><hr className='text-slate-400'/>
+				                    Amount With Tax: <span><i class="fa fa-inr" >{invoice?.subTotal || 0}</i></span>
                                 </td>
                             </tr>
                         </tbody>
@@ -197,12 +209,13 @@ export const BillsOfSupply = () => {
                     </table>
                 </div>
             </div>
+            }
+            </div>
             <div className="flex justify-center gap-8 py-4">
                 <button onClick={()=> navigate('/dashboard/create-invoice')} className='text-white p-2 px-4 cursor-pointer font-semibold rounded-xs shadow-md bg-green-600 hover:bg-green-700'>Create New Invoice</button>
                 <button className='text-white p-2 px-4 cursor-pointer font-semibold rounded-xs shadow-md bg-blue-700 hover:bg-blue-800'>Edit Invoice</button>
                 <button onClick={()=> navigate('/dashboard/invoice-list')} className='text-white p-2 px-4 cursor-pointer font-semibold rounded-xs shadow-md bg-cyan-500 hover:bg-cyan-600'>Invoice List</button>
             </div>
-        </div>
     </div>
   )
 }
