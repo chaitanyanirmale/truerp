90c8ba9173ff398f827e1fc8c2907f6e5ebcb453
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-08 15:08:14 +0530

    added route for get item and update item

--- api/routes/item.route.js
+++ api/routes/item.route.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import { addItem, addSupplierProduct, getItems, getSupplierProductList, previewItemCode } from '../controllers/item.controller.js'
+import { addItem, addSupplierProduct, getItem, getItems, getSupplierProductList, previewItemCode, updateItem } from '../controllers/item.controller.js'
 
 const router =  express.Router()
 
@@ -7,6 +7,8 @@ router.post('/add-item', addItem);
 router.post('/supplier-product', addSupplierProduct);
 router.get('/previewItemCode/:prefix', previewItemCode)
 router.get('/list', getItems)
+router.get('/item/:id', getItem);
 router.get('/sp-list', getSupplierProductList)
+router.put('/update-item/:id', updateItem);
 
 export default router;
