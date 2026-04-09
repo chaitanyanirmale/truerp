b9b6a425e486aa8dcdff4b6b477cc70dab3c6d8c
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-09 12:08:14 +0530

    Costing and Profit Prediction page complete

---
+++ client/src/pages/AI/CostingAndProfit.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+export const CostingAndProfit = () => {
+  const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false); 
+    const navigate = useNavigate(); 
+
+    const fetchItems = async () => {
+          setLoading(true);
+          try {
+              const res = await fetch('/api/items/list');
+              const data = await res.json();
+              if(data.success === true){
+                  setItems(data.data)
+              }
+              setLoading(false);
+          } catch (error) {
+              console.log(error)
+              setLoading(false);
+          }
+      }
+      useEffect(() => {
+        fetchItems();
+      }, []);
+  return (
+    <div className='p-4 bg-white rounded-sm shadow-sm'>
+        <div className="flex justify-between items-center">
+            <h1 className='text-2xl font-semibold'>💰 AI Costing & Profit Prediction</h1>
+            <button onClick={()=> navigate('/dashboard/aifeatures')} className='bg-rose-600 p-2 px-4 font-semibold text-white rounded-sm hover:bg-rose-700 hover:shadow-md'>Back</button>
+        </div>
+        <hr className='text-slate-300 my-4'/>
+        <div className="">
+            <table className='w-full text-center text-sm'>
+                <thead>
+                    <tr className='bg-green-800 text-white'>
+                        <th className='p-2 py-3 border-r border-slate-300  w-1/2'>Product</th>
+                        <th className='p-2 py-3 border-r border-slate-300'>Avg Purchase Cost</th>
+                        <th className='p-2 py-3 border-r border-slate-300'>Selling Price</th>
+                        <th className='p-2 py-3 border-r border-slate-300'>Extra Cost</th>
+                        <th className='p-2 py-3 border-r border-slate-300'>Total Cost</th>
+                        <th className='p-2 py-3 border-r border-slate-300'>Profit / Unit</th>
+                        <th className='p-2 py-3 border-r border-slate-300'>Margin %</th>
+                        <th className='p-2 py-3 w-30'>Ai Suggestion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item)=> (
+                        <tr key={item._id} className='border-b border-slate-300'>
+                            <td className='p-2 py-3'>{item.itemName}</td>
+                            <td className='p-2 py-3'>0</td>
+                            <td className='p-2 py-3'>0</td>
+                            <td className='p-2 py-3'>0</td>
+                            <td className='p-2 py-3'>0</td>
+                            <td className='p-2 py-3'>0</td>
+                            <td className='p-2 py-3'>0%</td>
+                            <td className='p-2 py-3'><span className='bg-rose-600 px-2 py-1 rounded-lg text-white font-semibold text-xs'>Increase Price</span></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </div>
+    )
+}
