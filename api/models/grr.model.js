1282a4a7e43e860bfc550536b0f0e39c418408c1
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-10 12:23:11 +0530

    added some new fields to grr model

--- api/models/grr.model.js
+++ api/models/grr.model.js
@@ -8,7 +8,47 @@ const grrSchema = mongoose.Schema({
     grrDate: {
         type: Date,
         required: true,
-    }
+    },
+    poId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PO",
+      required: true,
+    },
+
+    soId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SO",
+      required: true,
+    },
+
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true,
+    },
+
+    receivedQty: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    rejectedQty: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    acceptedQty: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    remark: {
+      type: String,
+      trim: true,
+    },
 },{ timestamps: true });
 
 const GRR = mongoose.model("GRR", grrSchema);
