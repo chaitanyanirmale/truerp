95a096eac5e0db98827cd2251f01264e326a245d
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-16 11:18:00 +0530

    route added

--- api/routes/salary.route.js
+++ api/routes/salary.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createSalary, deleteSalary, getSalaries } from "../controllers/salary.controller.js";
+import { createSalary, deleteSalary, getSalaries, getSalaryByEmployee } from "../controllers/salary.controller.js";
 
 
 const router = express.Router();
 
 router.post('/create', createSalary);
 router.get('/salary-list', getSalaries);
+router.get('/emp/:id', getSalaryByEmployee);
 router.delete("/delete-salary/:id", deleteSalary);
 
 export default router;
