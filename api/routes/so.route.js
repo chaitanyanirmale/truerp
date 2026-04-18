ccf41a1431ccf1db38fd9b709af42b6a691d213d
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-04-18 09:37:56 +0530

    route for get so by id added

--- api/routes/so.route.js
+++ api/routes/so.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createSO, getSO, previewJCNumber, previewSONumber } from '../controllers/so.controller.js';
+import { createSO, getSO, getSoById, previewJCNumber, previewSONumber } from '../controllers/so.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
 router.post('/create',verifyToken, createSO);
 router.get('/so-list', getSO);
+router.get('/:id', getSoById);
 router.get('/previewJCNumber', previewJCNumber);
 router.get('/previewSONumber', previewSONumber);
 
