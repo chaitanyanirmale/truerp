e22803446cce1882d517dfeec8762e1fe511d113
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-05-01 09:16:25 +0530

    Update So route added

--- api/routes/so.route.js
+++ api/routes/so.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addBOMItem, createSO, getSO, getSoById, previewJCNumber, previewSONumber } from '../controllers/so.controller.js';
+import { addBOMItem, createSO, getSO, getSoById, previewJCNumber, previewSONumber, updateSo } from '../controllers/so.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
@@ -7,6 +7,7 @@ const router = express.Router()
 router.post('/create',verifyToken, createSO);
 router.get('/so-list', getSO);
 router.get('/:id', getSoById);
+router.put('/update/:id', updateSo);
 router.get('/preview/JC-Number', previewJCNumber);
 router.get('/preview/SO-Number', previewSONumber);
 router.post('/so/:id/bom', addBOMItem);
