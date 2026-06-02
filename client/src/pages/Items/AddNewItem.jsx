79b71a9b4cc3cc5445a2b037f0126edb2d530026
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-06-02 09:25:19 +0530

    Add New Item Page Updated

--- client/src/pages/Items/AddNewItem.jsx
+++ client/src/pages/Items/AddNewItem.jsx
@@ -110,33 +110,63 @@ export const AddNewItem = () => {
   return (
     
     <div className='p-4 bg-white border border-slate-300 rounded-sm shadow-sm'>
-        <div className="flex justify-between">
-            <h1 className='text-2xl font-semibold'>Add New Item</h1>
-            <button onClick={()=> navigate('/dashboard/items-list')} className='p-1 px-4 bg-blue-800 text-white font-semibold rounded-sm'>List Item</button>
-        </div>
+        <h1 className='text-2xl font-semibold'>Add New Item</h1>    
         <hr className='my-4 text-slate-300'/>
         <form onSubmit={handleSubmit}>
-            <div className="grid xl:grid-cols-3 gap-8 items-stretch">
-                <div className="">
-                    <label className="text-sm font-semibold">Select Main Category</label>
-                    <select name="mainCategory" value={formData.mainCategory} onChange={handleChange} className="border border-slate-300 p-2 mt-1 rounded-sm w-full">
+            <h3 className='text-md bg-gray-200 border-l-4 border-blue-700 p-2 font-semibold rounded-r-sm'><i className='fa fa-th-large pr-2'></i>Category</h3>
+            <div className="grid grid-cols-3 gap-8 p-2 my-2 text-sm">
+                <div className="grid gap-2">
+                    <label className="text-sm font-semibold">Main Category</label>
+                    <select name="mainCategory" value={formData.mainCategory} onChange={handleChange} className="border border-slate-300 p-2 rounded-sm w-full">
                         <option value="">-- Select Main Category --</option>
                         {mainCategories.map((mainCat)=>(
                             <option key={mainCat._id} value={mainCat._id}>{mainCat.name}</option>
                         ))}
                     </select>
                 </div>
-                <div className="">
-                    <label className="text-sm font-semibold">Select Sub Category</label>
-                    <select name="subCategory" value={formData.subCategory} onChange={handleChange} className="border border-slate-300 p-2 mt-1 rounded-sm w-full">
+                <div className="grid gap-2">
+                    <label className="text-sm font-semibold">Sub Category</label>
+                    <select name="subCategory" value={formData.subCategory} onChange={handleChange} className="border border-slate-300 p-2 rounded-sm w-full">
                         <option value="">-- Select Sub Category --</option>
                         {subCategories.map((subCat)=>(
                             <option key={subCat._id} value={subCat._id}>{subCat.name}</option>
                         ))}
                     </select>
                 </div>
-                <div className="grid xl:grid-cols-2 gap-8">
-                    <div className="grid">
+                <div className="grid gap-2">
+                        <label className="text-sm font-semibold">Item Type</label>
+                        <select name="itemType" value={formData.itemType} onChange={handleChange} className="border border-slate-300 p-2 rounded-sm w-full">
+                            <option value="">-- Select --</option>
+                            <option value="Item">Item</option>
+                            <option value="Raw Material">Raw Material</option>
+                        </select>
+                    </div>
+            </div>
+            <h3 className='text-md bg-gray-200 border-l-4 border-green-500 p-2 font-semibold rounded-r-sm'><i className='fa fa-cube pr-2'></i>Item Details</h3>
+            <div className="grid grid-cols-4 gap-6 p-2 my-2 text-sm">
+                <div className="grid">
+                    <label className="text-sm font-semibold">Item Name/Model Name</label>
+                    <input type="text" name="itemName" value={formData.itemName} onChange={handleChange} className="border border-slate-300 p-2 w-full rounded-sm mt-2" placeholder='Item Name or Model Name'/>
+                </div>
+                <div className="grid">
+                    <label className="text-sm font-semibold">Unit*</label>
+                    <select name="perUnit" value={formData.perUnit} onChange={handleChange} className="border border-slate-300 p-2 rounded-sm mt-2">
+                        <option value="">-- Select Unit --</option>
+                        <option value="NOS">NOS</option>
+                        <option value="PC">PC</option>
+                        <option value="MTR">MTR</option>
+                        <option value="KG">KG</option>
+                    </select>
+                </div>
+                <div className="grid">
+                    <label className="text-sm font-semibold">Weight (Grams)</label>
+                    <input type="text" name="weightInGram" value={formData.weightInGram} onChange={handleChange} className="border border-slate-300 p-2  rounded-sm mt-2 w-full" placeholder='e.g. 500'/>
+                </div>
+                <div className="grid">
+                    <label className="text-sm font-semibold">Minimum Order Qty</label>
+                    <input type="text" name="minOrderQty" value={formData.minOrderQty} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" placeholder='Minimum Order QTY'/>
+                </div>
+                <div className="grid">
                         <label className="text-sm font-semibold">Check Last Item Code</label>
                         <select name="prefix" onChange={handlePrefixChange} className='border border-slate-300 p-2 mt-1 rounded-sm w-full'>
                             <option value="">--Select Prefix--</option>
@@ -153,68 +183,37 @@ export const AddNewItem = () => {
                         {formData.itemCode &&
                             <p className='text-blue-700 text-sm font-semibold mt-1'>Last Item Code : {formData.itemCode}</p>
                         }
-                    </div>
-                    <div className="grid items-start">
-                        <label className="text-sm font-semibold">Item Code</label>
-                        <input type="text" name="itemCode" value={formData.itemCode} className="border border-slate-300 p-2 mt-1 rounded-sm w-full" readOnly/>
-                    </div>
                 </div>
-            </div>
-            <div className="grid xl:grid-cols-3 mt-4 gap-8">
-                <div className="grid">
-                    <label className="text-sm font-semibold">Item Name/Model Name</label>
-                    <input type="text" name="itemName" value={formData.itemName} onChange={handleChange} className="border border-slate-300 p-2  rounded-sm mt-2" />
-                </div>
-                <div className="grid">
-                    <label className="text-sm font-semibold">Per Unit*</label>
-                    <select name="perUnit" value={formData.perUnit} onChange={handleChange} className="border border-slate-300 p-2 rounded-sm mt-2">
-                        <option value="">-- Select Unit --</option>
-                        <option value="NOS">NOS</option>
-                        <option value="PC">PC</option>
-                        <option value="MTR">MTR</option>
-                        <option value="KG">KG</option>
-                    </select>
-                </div>
-                <div className="grid xl:grid-cols-2 gap-8">
-                    <div className="grid">
-                        <label className="text-sm font-semibold">Weight in Gram</label>
-                        <input type="text" name="weightInGram" value={formData.weightInGram} onChange={handleChange} className="border border-slate-300 p-2  rounded-sm mt-2 w-full" />
-                    </div>
-                    <div className="grid">
-                        <label className="text-sm font-semibold">Item or Raw Material</label>
-                        <select name="itemType" value={formData.itemType} onChange={handleChange} className="border border-slate-300 p-2 rounded-sm mt-2 w-full">
-                            <option value="">-- Select --</option>
-                            <option value="Item">Item</option>
-                            <option value="Raw Material">Raw Material</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div className="grid xl:grid-cols-4 mt-4 gap-8">
-                <div className="grid">
-                    <label className="text-sm font-semibold">Item Price Supplier</label>
-                    <input type="text" name="supplierPrice" value={formData.supplierPrice} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" />
-                </div>
-                <div className="grid">
-                    <label className="text-sm font-semibold">Item Price Customer</label>
-                    <input type="text" name="customerPrice" value={formData.customerPrice} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" />
-                </div>
-                <div className="grid">
-                    <label className="text-sm font-semibold">Minimum Order Qty</label>
-                    <input type="text" name="minOrderQty" value={formData.minOrderQty} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" />
+                <div className="grid items-start">
+                    <label className="text-sm font-semibold">Item Code</label>
+                    <input type="text" name="itemCode" value={formData.itemCode} className="border border-slate-300 p-2 mt-1 rounded-sm w-full" readOnly/>
                 </div>
                 <div className="grid">
                     <label className="text-sm font-semibold">Stock Count</label>
-                    <input type="text" name="stockCount" value={formData.stockCount} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" />
+                    <input type="text" name="stockCount" value={formData.stockCount} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" placeholder='e.g. 100'/>
                 </div>
             </div>
-            <div className="grid mt-4">
+            <h3 className='text-md bg-gray-200 border-l-4 border-yellow-400 p-2 font-semibold rounded-r-sm'><i className='fa fa-inr pr-2'></i>Pricing</h3>
+            <div className="grid grid-cols-2 gap-6 p-2 my-2 w-1/2 text-sm">
+                <div className="grid">
+                    <label className="text-sm font-semibold">Supplier Price</label>
+                    <input type="number" name="supplierPrice" value={formData.supplierPrice} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" placeholder='0.00'/>
+                </div>
+                <div className="grid">
+                    <label className="text-sm font-semibold">Customer Price</label>
+                    <input type="number" name="customerPrice" value={formData.customerPrice} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" placeholder='0.00'/>
+                </div>
+            </div>
+            <h3 className='text-md bg-gray-200 border-l-4 border-purple-500 p-2 font-semibold rounded-r-sm'><i className='fa fa-align-left pr-2'></i>Description</h3>
+            <div className="grid my-2 p-2 text-sm">
                 <label className="text-sm font-semibold">Item Description</label>
-                <textarea name="description" value={formData.description} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" rows="2"></textarea>
+                <textarea name="description" value={formData.description} onChange={handleChange} className="border border-slate-300 rounded-sm p-2 mt-2" rows="2" placeholder='Enter the detailed description of item'></textarea>
             </div>
             <hr className='my-4 text-slate-300'/>
-            <div className="flex justify-end">
-                <button className='bg-blue-700 px-3 py-1 text-white rounded-sm '>Next</button>
+            <div className="flex gap-6 font-semibold">
+                <button className='bg-blue-700 px-3 py-1 text-white rounded-xs hover:scale-105 transition-all duration-700'><i className='fa fa-arrow-right pr-2'></i>Save & Next</button>
+                <button className='bg-gray-500 px-3 py-1 text-white rounded-xs hover:scale-105 transition-all duration-700' onClick={()=> setFormData(initialData)}><i className='fa fa-times-circle pr-2'></i>Clear</button>
+                <button onClick={()=> navigate('/dashboard/items-list')} className='p-1 px-4 text-blue-700 rounded-sm hover:scale-105 transition-all duration-700 hover:shadow-md'><i className='fa fa-list pr-2'></i> List Item</button>
             </div>
         </form>
         <div className="">
