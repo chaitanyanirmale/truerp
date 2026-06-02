94928dacb60a03d514b92ebb1aef7932aae1259c
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-06-02 08:29:51 +0530

    Content and UI Updated

--- client/src/pages/Expanse/ExpenseList.jsx
+++ client/src/pages/Expanse/ExpenseList.jsx
@@ -19,23 +19,8 @@ export const ExpenseList = () => {
       setLoading(false);
     }
   }
-  const fetchEmployees = async () => {
-      setLoading(true);
-      try {
-          const res = await fetch('/api/employee/emp-list');
-          const data = await res.json();
-          if(data.success === true){
-            setEmployees(data.data)
-          }
-          setLoading(false);
-      } catch (error) {
-          console.log(error)
-          setLoading(false);
-      }
-  }
-    
+      
   useEffect(()=> {
-    fetchEmployees();
     fetchExpenses();
   },[])
 
@@ -60,33 +45,27 @@ export const ExpenseList = () => {
   );
   return (
     <div  className='p-4 bg-white shadow-md rounded-sm'>
-      <div className='flex justify-between'>
-        <h4 className='mb-4 text-2xl font-semibold'>List of Expenses</h4>
-        <div className="">
-            <select type="text" name="" className="border border-slate-300 rounded-sm p-2 bg-white text-sm">
-              <option value="">---Select Employee---</option>
-              {employees.map((emp) => (
-                <option key={emp._id} value={emp._id}>
-                {emp.fullname}
-                </option>
-              ))}
-            </select>
-        </div>
+    <h4 className='mb-4 text-2xl font-semibold'><i className='fa fa-file-text-o pr-2 text-blue-700'></i>List of Expenses</h4>
+    <hr className='text-slate-300 my-4'/>
+    <div className="m-2 text-sm border border-slate-300 p-4 rounded-sm"> 
+      <div className="flex justify-between">
+        <h2 className='text-md font-semibold uppercase'><i className='fa fa-table pr-2 text-blue-700'></i>Expense Records</h2>
+        <p className='p-1 px-3 text-blue-700 bg-blue-100 font-semibold rounded-full'>{expenses?.length || 0} Records</p> 
       </div>
-      <hr className='text-slate-300 my-4'/>
-    <div className="m-2 text-sm">      
+        <hr className='text-slate-300 my-4'/>
           {loading ? (
             <p>Loading..</p>
           ) : <div className="text-center">
             <table className='p-2 mt-4 w-full'>
-              <thead className='border border-slate-300 bg-blue-700 text-white'>
+              <thead className='border border-slate-300 '>
                 <tr className=''>
-                  <th className='p-4 border border-slate-300'>Full Name</th>
-                  <th className='p-4 border border-slate-300'>Expense Date</th>
-                  <th className='p-4 border border-slate-300'>Expense Amount</th>
-                  <th className='p-4 border border-slate-300'>Expense Type</th>
-                  <th className='p-4 border border-slate-300'>Expense Note</th>
-                  <th className='p-4 border border-slate-300'>Action</th>
+                  <th className='p-2 border border-slate-300 w-15'>Sr. No.</th>
+                  <th className='p-2 border border-slate-300'>Date</th>
+                  <th className='p-2 border border-slate-300'>Employee</th>
+                  <th className='p-2 border border-slate-300'>Type</th>
+                  <th className='p-2 border border-slate-300'>Note</th>
+                  <th className='p-2 border border-slate-300'>Amount</th>
+                  <th className='p-2 border border-slate-300'>Action</th>
                 </tr>
               </thead>
               <tbody>
@@ -95,16 +74,16 @@ export const ExpenseList = () => {
                     <td className='p-2 border border-slate-300' colSpan="6">No Expenses Found</td>
                   </tr>
                   
-                ) : expenses.map((exp)=> 
-                <tr key={exp._id} className=''>
+                ) : expenses.map((exp, index)=> 
+                <tr key={exp._id} className='text-start'>
+                  <td className='p-2 border border-slate-300 text-center'>{index + 1}</td>
+                  <td className='p-2 border border-slate-300'>{new Date(exp.expenseDate).toISOString().split("T")[0]}</td>
                   <td className='p-2 border border-slate-300'>{exp.employee?.fullname}</td>
-                  <td className='p-2 border border-slate-300'>{new Date(exp.expenseDate).toLocaleDateString()}</td>
-                  <td className='p-2 border border-slate-300'>{exp.amount}</td>
-                  <td className='p-2 border border-slate-300'>{exp.expenseType}</td>
+                  <td className='p-2 border border-slate-300'><span className="border border-gray-300 p-1 px-3 rounded-sm bg-slate-200 font-semibold">{exp.expenseType}</span></td>
                   <td className='p-2 border border-slate-300'>{exp.note}</td>
-                  {/* <td className='p-2 border border-slate-400'><a href="" className='bg-yellow-500 px-2 py-1 rounded-sm '><i className="fa fa-plus px-1" aria-hidden="true"></i>Attach file</a></td> */}
-                  <td className='p-2 border border-slate-300'>
-                    <button onClick={() => deleteExpense(exp._id)} className='bg-rose-600 p-2 rounded-sm text-white hover:bg-rose-700 hover:scale-105 transition-all duration-700'><i className="fa fa-trash px-2"></i>Delete</button>
+                  <td className='p-2 border border-slate-300 text-blue-700 font-semibold'>Rs. {exp.amount}</td>
+                  <td className='p-2 border border-slate-300 text-center'>
+                    <button onClick={() => deleteExpense(exp._id)} className='text-rose-600 p-2 border border-rose-600 rounded-sm hover:bg-rose-700 hover:text-white hover:scale-110 transition-all duration-300'><i className="fa fa-trash px-2"></i>Delete</button>
                   </td>
                 </tr>)
                 }             
@@ -113,16 +92,15 @@ export const ExpenseList = () => {
           </div>
           }       
     </div>
-        <div className="bg-white shadow-sm">
-            <table className='border border-slate-300 mt-4 w-full'>
-              <thead className='w-full'>
-                <tr><th className='p-2 text-blue-600'>Total Expenses</th></tr>
-              </thead>
-              <tbody className='w-full text-center border border-slate-300'>
-                <tr><td className='p-2 font-bold'>Rs. {totalExpense}</td></tr>
-              </tbody>
-            </table>
-          </div>  
+      <div className="bg-blue-600 text-white flex justify-between items-center p-4 font-semibold rounded-md">
+        <div className="flex">
+          <i className='fa fa-calculator pr-2 mt-1'></i>
+          <h1>Total Expenses</h1>
+        </div>
+        <div className="text-xl">
+          Rs. {totalExpense}.00/-
+        </div>
+      </div>  
   </div>
   )
 }
